3ad8ed74c204e323976cda98bdb7b9ea5209c162
Vadim Filimonov <fffilimonov@yandex.ru>
2019-09-07 00:06:56 +0300

    use mail

--- services/mail_service.go
+++ services/mail_service.go
@@ -3,7 +3,7 @@ package services
 import (
 	"../services/models"
 	b64 "encoding/base64"
-	// . "github.com/mailjet/mailjet-apiv3-go"
+	. "github.com/mailjet/mailjet-apiv3-go"
 	"net/http"
 	"fmt"
 )
@@ -12,23 +12,23 @@ func SendActivation(requestUser *models.User) (int, []byte) {
 
 	sEnc := b64.URLEncoding.EncodeToString([]byte(requestUser.Username))
 
-	// mailjetClient := NewMailjetClient("", "")
-	// email := &InfoSendMail{
-	// 	FromEmail: "admin@likeuser.com",
-	// 	FromName:  "Activate Mail",
-	// 	Subject:   "Activate Likeuser.com account",
-	// 	TextPart:  "Hello.\nLink for activation:\n" + "http://likeuser.com/activate/" + sEnc + "\nThank you.",
-	// 	Recipients: []Recipient{
-	// 		Recipient{
-	// 			Email: requestUser.Username,
-	// 		},
-	// 	},
-	// }
-	//_, err := mailjetClient.SendMail(email)
-		fmt.Printf("link: %v\n", "http://likeuser.com/activate/" + sEnc)
-	// if err != nil {
-	// 	panic(err)
-	// } else {
+	mailjetClient := NewMailjetClient("", "")
+	email := &InfoSendMail{
+		FromEmail: "admin@likeuser.com",
+		FromName:  "Activate Mail",
+		Subject:   "Activate Likeuser.com account",
+		TextPart:  "Hello.\nLink for activation:\n" + "https://likeuser.com/activate/" + sEnc + "\nThank you.",
+		Recipients: []Recipient{
+			Recipient{
+				Email: requestUser.Username,
+			},
+		},
+	}
+	_, err := mailjetClient.SendMail(email)
+	fmt.Printf("link: %v\n", "https://likeuser.com/activate/" + sEnc)
+	if err != nil {
+		panic(err)
+	} else {
 		return http.StatusOK, []byte("")
-	// }
+	}
 }
